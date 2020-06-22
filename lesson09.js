b61a4cac225167a0069112839690de32abb98d8d
Dmitry Shimansky <Shimansky.Dmitry@gmail.com>
2020-06-23 00:16:46 +0300

    Выполненное задание по Уроку 9

---
+++ lesson09.js
@@ -0,0 +1,67 @@
+'use strict';
+
+let start = document.getElementById('start');
+console.log(start);
+
+let plusIncome = document.getElementsByTagName('button')[0];
+console.log(plusIncome);
+
+let plusExpensesAdd = document.getElementsByClassName('btn_plus expenses_add')[0];
+console.log(plusExpensesAdd);
+
+let checkBox = document.querySelector('#deposit-check');
+console.log(checkBox);
+
+let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+console.log(additionalIncomeItem);
+
+let budgetDay = document.getElementsByClassName('result-total budget_day-value')[0];
+console.log(budgetDay);
+
+let expensesMonth = document.getElementsByClassName('expenses_month-value')[0];
+console.log(expensesMonth);
+
+let additionalIncome = document.getElementsByClassName('additional_income-value')[0];
+console.log(additionalIncome);
+
+let additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0];
+console.log(additionalExpenses);
+
+let incomePeriod = document.getElementsByClassName('income_period-value')[0];
+console.log(incomePeriod);
+
+let targetMonth = document.getElementsByClassName('target_month-value')[0];
+console.log(targetMonth);
+
+let budgetMonth = document.querySelector('.budget_month-value');
+console.log(budgetMonth);
+
+let period = document.querySelector('.period-select');
+console.log(period);
+
+let targetAmount = document.querySelector('.target-amount');
+console.log(targetAmount);
+
+let depositAmount = document.querySelector('.deposit-amount');
+console.log(depositAmount);
+
+let depositPercent = document.querySelector('.deposit-percent');
+console.log(depositPercent);
+
+let additionalExpItem = document.querySelector('.additional_expenses-item');
+console.log(additionalExpItem);
+
+let expensesAmount = document.querySelector('.expenses-amount');
+console.log(expensesAmount);
+
+let expensesTitle = document.querySelector('input.expenses-title');
+console.log(expensesTitle);
+
+let incomeAmount = document.querySelector('.income-amount');
+console.log(incomeAmount);
+
+let incomeTitle = document.querySelector('input.income-title');
+console.log(incomeTitle);
+
+let salaryAmount = document.querySelector('.salary-amount');
+console.log(salaryAmount);
